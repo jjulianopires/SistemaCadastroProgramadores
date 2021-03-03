784960a683f05fe36d0cd047f81748f97d6ebd00
jjulianopires <jjulianopires@gmail.com>
2021-03-03 20:54:54 -0300

    form login
    Criação do Form de Login

--- frontend/src/pages/Login/LoginComponent.vue
+++ frontend/src/pages/Login/LoginComponent.vue
@@ -2,16 +2,55 @@
   <div class="main">
     <div class="login">
       <div class="content-login">
-          <header>
-              <h1>Login do Sistema</h1>
-          </header>
+        <header>
+          <h1>Login do Sistema</h1>
+        </header>
+
+        <div class="form">
+          <form>
+            <div class="form-group">
+              <label for="exampleInputEmail1">E-mail</label>
+              <input
+                type="email"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="E-mail"
+              />
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Senha</label>
+              <input
+                type="password"
+                class="form-control"
+                id="exampleInputPassword1"
+                placeholder="Senha"
+              />
+            </div>
+            <button type="submit" class="btn btn-primary" @click="login">
+              Entrar
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: 'LoginComponent',
+
+  data() {
+    return {};
+  },
+
+  methods: {
+    login() {
+      this.$router.push({ name: "home" });
+    },
+  },
+};
 </script>
 
 <style lang="scss" src="./style.scss" scoped />
