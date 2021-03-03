2c7c6520d0c15c6e119fbc50570bc6ff65a5d9ab
jjulianopires <jjulianopires@gmail.com>
2021-03-02 23:38:49 -0300

    Implementação bac-kend do login e logout via API.
    -> Implementação da parte do back-end do login (email e senha) e logout via API.
    -> Utilização do JWT (JSON Web Token) para transferência de dados de maneira “segura”.
    -> Implementação do método para atualizar (refresh) o token de acesso. O token é gerado com duração de 24 horas.
    -> Criação de uma factory para gerar registros de recrutadores para testes.
    -> Implementação de validações de acordo com as funcionalidades implementadas.

---
+++ laravel/app/Http/Controllers/AuthController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+
+class AuthController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login']]);
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'email'    => 'required|email',
+                'password' => 'required|string|min:6',
+            ]
+        );
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        //token time day
+        $token_validity = (24 * 60);
+
+        $this->guard()->factory()->setTTL($token_validity);
+
+        if (!$token = $this->guard()->attempt($validator->validated())) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json(
+            [
+                'token'          => $token,
+                'token_type'     => 'bearer',
+                'token_validity' => ($this->guard()->factory()->getTTL() * 60),
+            ]
+        );
+    }
+
+
+    public function logout()
+    {
+        $this->guard()->logout();
+
+        return response()->json(['message' => 'User logged out successfully']);
+    }
+
+    public function refresh()
+    {
+        return $this->respondWithToken($this->guard()->refresh());
+
+    }
+
+    protected function guard()
+    {
+        return Auth::guard();
+    }
+}
