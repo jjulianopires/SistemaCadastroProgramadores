ca27c6ae25d4cc71005c3d38a49cb758f84a4626
jjulianopires <jjulianopires@gmail.com>
2021-03-03 20:27:42 -0300

    Configurações e estilização inicial da tela de login.
    ->Instalação do vue router
    ->Criação do LoginComponent
    ->Começo da implementação do Login no frontend
    ->Impotação do bootstrap
    
    OBS: Só para deixar registrado, não pude trabalhar no projeto hoje no turno da tarde.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import LoginComponent from './pages/Login/LoginComponent'
+import HomeComponent from './pages/Home/HomeComponent'
+import "bootstrap/dist/css/bootstrap.min.css"
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
 
+//cria uma constante com as rotas
+const routes = [
+  { path: '/', name: 'login', component: LoginComponent },
+  { path: '/home', name: 'home', component: HomeComponent }
+]
+
+//usa a instancia de VueRouter
+const router = new VueRouter({
+  routes
+})
+
+//disponibiliza router para o Vue
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ frontend/src/pages/Login/LoginComponent.vue
@@ -0,0 +1,17 @@
+<template >
+  <div class="main">
+    <div class="login">
+      <div class="content-login">
+          <header>
+              <h1>Login do Sistema</h1>
+          </header>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" src="./style.scss" scoped />
